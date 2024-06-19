9ee6b05a35f91caf639b75de6cc241d4dd530dba
Sidhartha Sankar Swain <sidharthasankarswain425@gmail.com>
2024-06-19 03:07:15 -0400

    Added axios and await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const axios = require("axios");
+
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -21,39 +23,47 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', (req, res) => {
-    res.send(JSON.stringify({books}, null, 4));
+public_users.get('/', async (req, res) => {
+    const response = await axios.get("https://raw.githubusercontent.com/sidhartha426/expressBookReviews/main/final_project/router/books.json");
+    const books = response.data;
+    res.send(JSON.stringify({ books }, null, 4));
 });
 
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', (req, res) => {
+public_users.get('/isbn/:isbn',async (req, res) => {
     const isbn = req.params.isbn;
+    const response = await axios.get("https://raw.githubusercontent.com/sidhartha426/expressBookReviews/main/final_project/router/books.json");
+    const books = response.data;
     res.send(books[isbn]);
 });
 
 // Get book details based on author
-public_users.get('/author/:author', (req, res) => {
+public_users.get('/author/:author',async (req, res) => {
     const author = req.params.author;
+    const response = await axios.get("https://raw.githubusercontent.com/sidhartha426/expressBookReviews/main/final_project/router/books.json");
+    const books = response.data;
     const booksByAuthor = [];
     for (const isbn in books) {
         if (books[isbn]["author"] === author) {
             booksByAuthor.push(books[isbn]);
         }
     }
-    res.send(JSON.stringify({booksbyauthor:booksByAuthor}, null, 4));
+    res.send(JSON.stringify({ booksbyauthor: booksByAuthor }, null, 4));
 });
 
 // Get all books based on title
-public_users.get('/title/:title', (req, res) => {
+public_users.get('/title/:title',async (req, res) => {
     const title = req.params.title;
+    const response = await axios.get("https://raw.githubusercontent.com/sidhartha426/expressBookReviews/main/final_project/router/books.json");
+    const books = response.data;
     const booksByTitle = [];
     for (const isbn in books) {
         if (books[isbn]["title"] === title) {
             booksByTitle.push(books[isbn]);
         }
     }
-    res.send(JSON.stringify({booksbytitle:booksByTitle}, null, 4));
+    res.send(JSON.stringify({ booksbytitle: booksByTitle }, null, 4));
 });
 
 //  Get book review
